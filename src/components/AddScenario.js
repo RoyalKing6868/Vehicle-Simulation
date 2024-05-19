b8f6896a47b635a1b51f9c20dc82a9af94ed0abd
Swarup Hande <71242592+RoyalKing6868@users.noreply.github.com>
2024-05-19 16:32:00 +0530

    added

--- src/components/AddScenario.js
+++ src/components/AddScenario.js
@@ -1,9 +1,32 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import "./addScenario.css"
 const AddScenario = () => {
+    const [scenarioText, setScenarioText] = useState("");
+    const [time, setTime] = useState();
+    const handleAdd = () =>{
+        setScenarioText("");
+        setTime();
+        console.log(scenarioText);
+        console.log(time);
+    }
   return (
     <>
-    <div>AddScenario</div>
+    <div>Add Scenario</div>
+    <div className='scenario'>
+        <div>
+            <p>Scenario Name</p>
+            <input type="text" value={scenarioText} onChange={(e)=>setScenarioText(e.target.value)}/>
+        </div>
+        <div>
+            <p>Scenario Time (seconds)</p>
+            <input type="text" value={time} onChange={(e)=>setTime(e.target.value)}/>
+        </div>
+    </div>
+    <div className='operations'>
+        <button className='btn' id='add' onClick={handleAdd}>Add</button>
+        <button className='btn' id='reset'>Reset</button>
+        <button className='btn' id='back'>Go Back</button>
+    </div>
     </>
   )
 }
--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,8 +1,16 @@
 import React from 'react'
-
+import "./homepage.css"
 const Homepage = () => {
   return (
-    <div>Homepage</div>
+    <>
+        <div>Homepage</div>
+        <div className='custom-select'>
+            <select>
+                <option>Scenario</option>
+                <option>Scenario2</option>
+            </select>
+        </div>  
+    </>
   )
 }
 
